26be7d5bc64b33ac7480fee5110fc843c7ee0206
Maciej Chałapuk <m.chalapuk@quedex.net>
2018-05-24 17:41:31 +0200

    + default export in index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-export { FileHandler } from './FileHandler';
+export { FileHandler, FileHandler as default } from './FileHandler';
 export { Builder } from './Builder';
 export { Formatter } from './Formatter';
 export { LogstashFormatter } from './LogstashFormatter';
