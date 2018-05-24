e67595ff43bcb2ef9a0d7bb1d581af3ca951c01b
Maciej Chałapuk <m.chalapuk@quedex.net>
2018-05-24 17:40:31 +0200

    + index

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export { FileHandler } from './FileHandler';
+export { Builder } from './Builder';
+export { Formatter } from './Formatter';
+export { LogstashFormatter } from './LogstashFormatter';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { FileHandler } from './FileHandler';
+export { Builder } from './Builder';
+export { Formatter } from './Formatter';
+export { LogstashFormatter } from './LogstashFormatter';
+
