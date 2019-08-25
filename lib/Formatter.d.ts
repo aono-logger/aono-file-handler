63c828eb7e886a895f062a5cfe42e28fc3888594
Maciej Chałapuk <maciej@chalapuk.pl>
2019-08-25 13:23:30 +0200

    - lib folder

--- lib/Formatter.d.ts
+++
@@ -1,8 +0,0 @@
-import { Entry } from 'aono';
-/**
- * @author Maciej Chałapuk (maciej@chalapuk.pl)
- */
-export interface Formatter {
-    format(entry: Entry): string;
-}
-export default Formatter;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { FileHandler, FileHandler as default } from './FileHandler';
-export { Builder } from './Builder';
-export { Formatter } from './Formatter';
-export { LogstashFormatter } from './LogstashFormatter';
