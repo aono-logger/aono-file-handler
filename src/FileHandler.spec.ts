419eef4ee4856aaa89044ac02153c6c1c563ce9d
Maciej Chałapuk <m.chalapuk@quedex.net>
2020-01-06 09:59:05 +0100

    ~ rewritten tests of FileHandler in typescript

---
+++ src/FileHandler.spec.ts
@@ -0,0 +1,211 @@
+
+import * as fs from 'fs';
+import * as util from 'util';
+import * as glob from 'glob';
+
+import * as should from 'should';
+
+import FileHandler from './FileHandler';
+import LogstashFormatter from './LogstashFormatter';
+
+describe('FileHandler', () => {
+  const prefix = '/tmp/aono-file-handler/test.log';
+
+  let testedHandler : FileHandler;
+
+  beforeEach(done => {
+    glob(`${prefix}*`, (err, files) => {
+      if (err) {
+        done(err);
+        return;
+      }
+      files.forEach(file => fs.unlinkSync(file));
+      done();
+    });
+  });
+
+  describe('after creation', () => {
+    beforeEach(() => {
+      testedHandler = new FileHandler(prefix);
+    });
+
+    it('contains passed parameters', () => {
+      testedHandler.prefix.should.equal(prefix);
+    });
+    it('contains default rotation threshold', () => {
+      testedHandler.rotationBytesThreshold.should.equal(104857600);
+    });
+
+    it('contains null currentFile', () => {
+      should(testedHandler.currentFile).equal(null);
+    });
+    it('contains zeroed currentFileSize', () => {
+      should(testedHandler.currentFileSize).equal(null);
+    });
+    it('contains zero bytesWritten', () => {
+      testedHandler.bytesWritten.should.equal(0);
+    });
+
+    describe('when after handling log entry without meta', () => {
+      const entry = {
+        timestamp: 0,
+        logger: 'test',
+        level: 'good',
+        message: 'hello, file!',
+        meta: {},
+      };
+
+      beforeEach(() => {
+        return testedHandler.write([ entry ]);
+      });
+
+      it('contains properly set currentFile', () => {
+        should(testedHandler.currentFile).equal(`${prefix}.1970-01-01_00:00:00.000`);
+      });
+      it('contains properly set currentFileSize', () => {
+        should(testedHandler.currentFileSize).equal(106);
+      });
+      it('contains properly set bytesWritten', () => {
+        testedHandler.bytesWritten.should.equal(106);
+      });
+
+      it('wrote log entry to a log file', () => {
+        const contents = fs.readFileSync(testedHandler.currentFile as string)
+          .toString('utf-8')
+        ;
+        contents.should.equal('{ '+
+          '"timestamp": "1970-01-01T00:00:00.000Z", '+
+          '"logger": "test", '+
+          '"level": "good", '+
+          '"message": "hello, file!"'+
+          ' }\n')
+        ;
+      });
+    });
+
+    describe('when after handling log entry with meta', () => {
+      const entry = {
+        timestamp: 0,
+        logger: 'test',
+        level: 'good',
+        message: 'hello, file!',
+        meta: {
+          number: 1,
+        },
+      };
+
+      beforeEach(() => {
+        return testedHandler.write([ entry ]);
+      });
+
+      it('contains properly set currentFile', () => {
+        should(testedHandler.currentFile).equal(`${prefix}.1970-01-01_00:00:00.000`);
+      });
+      it('contains properly set currentFileSize', () => {
+        should(testedHandler.currentFileSize).equal(124);
+      });
+      it('contains properly set bytesWritten', () => {
+        testedHandler.bytesWritten.should.equal(124);
+      });
+
+      it('wrote log entry to a log file', () => {
+        const contents = fs.readFileSync(testedHandler.currentFile as string)
+          .toString('utf-8')
+        ;
+        contents.should.equal('{ '+
+          '"timestamp": "1970-01-01T00:00:00.000Z", '+
+          '"logger": "test", '+
+          '"level": "good", '+
+          '"message": "hello, file!", '+
+          '"aono_number": 1'+
+          ' }\n'
+        );
+      });
+    });
+  });
+
+  describe('after creation with small rotation threshold', () => {
+    const smallThreshold = 16;
+
+    beforeEach(() => {
+      testedHandler = new FileHandler(prefix, new LogstashFormatter(), smallThreshold);
+    });
+
+    it('contains properly set rotation threshold', () => {
+      testedHandler.rotationBytesThreshold.should.equal(smallThreshold);
+    });
+
+    describe('when after handling first log entry', () => {
+      const entry0 = {
+        timestamp: 0,
+        logger: 'test',
+        level: 'good',
+        message: 'hello, file!',
+        meta: {},
+      };
+
+      beforeEach(() => {
+        return testedHandler.write([ entry0 ]);
+      });
+
+      it('contains null currentFile', () => {
+        should(testedHandler.currentFile).equal(null);
+      });
+      it('contains zeroed currentFileSize', () => {
+        should(testedHandler.currentFileSize).equal(null);
+      });
+
+      describe('and after adding second log entry', () => {
+        const entry1 = {
+          timestamp: 1,
+          logger: 'test',
+          level: 'bad',
+          message: 'it\'s you again',
+          meta: {},
+        };
+
+        beforeEach(() => {
+          return testedHandler.write([ entry1 ]);
+        });
+
+        it('contains null currentFile', () => {
+          should(testedHandler.currentFile).equal(null);
+        });
+        it('contains zeroed currentFileSize', () => {
+          should(testedHandler.currentFileSize).equal(null);
+        });
+
+        it('contains properly set bytesWritten', () => {
+          testedHandler.bytesWritten.should.equal(213);
+        });
+
+        it('wrote first log entry to first file', () => {
+          const contents = fs.readFileSync(`${prefix}.1970-01-01_00:00:00.000`)
+            .toString('utf-8')
+          ;
+          contents.should.equal('{ '+
+            '"timestamp": "1970-01-01T00:00:00.000Z", '+
+            '"logger": "test", '+
+            '"level": "good", '+
+            '"message": "hello, file!"'+
+            ' }\n')
+          ;
+        });
+
+        it('wrote second log entry to second file', () => {
+          const contents = fs.readFileSync(`${prefix}.1970-01-01_00:00:00.001`)
+            .toString('utf-8')
+          ;
+          contents.should.equal('{ '+
+            '"timestamp": "1970-01-01T00:00:00.001Z", '+
+            '"logger": "test", '+
+            '"level": "bad", '+
+            '"message": "it\'s you again"'+
+            ' }\n')
+          ;
+        });
+      });
+    });
+  });
+});
+
